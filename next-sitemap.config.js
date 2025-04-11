9d64940dcb223d7c5b6a3f2ec3489f2401a056dd
sunghyun JE <khaos1004@naver.com>
2025-04-11 14:34:07 +0900

    sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://jcodelab.shop',
-    generateRobotsTxt: true,
+    siteUrl: 'https://jcodelab.shop', // ← 본인 도메인
+    generateRobotsTxt: true, // robots.txt도 자동 생성됨
   };
   
